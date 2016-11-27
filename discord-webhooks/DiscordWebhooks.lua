f35cca67e2f6886bbcff7863572336fa97284d5f
Brad Sharp <BradSharp@users.noreply.github.com>
2016-11-27 22:34:11 +0000

    Create DiscordWebhooks.lua

---
+++ discord-webhooks/DiscordWebhooks.lua
@@ -0,0 +1,35 @@
+local httpService = game:GetService'HttpService'
+local domain = "https://discordapp.com/api/webhooks"
+
+local module = {}
+
+local hookMetatable = {
+	__index = {
+		ClassName = "DiscordWebhook",
+		GetAsync = function (self)
+			return httpService:GetAsync(domain .. "/" .. self.URL)
+		end,
+		PostAsync = function (self, content, username, avatar, tts, file, embeds)
+			return httpService:PostAsync(
+				domain .. "/" .. self.URL,
+				httpService:JSONEncode{
+					content = content,
+					username = username,
+					avatar_url = avatar,
+					tts = tts,
+					file = file,
+					embeds = embeds
+				}
+			)
+		end
+	},
+	__newindex = error
+}
+
+function module.RegisterHook(id, token) 
+	return setmetatable({
+		URL = id .. "/" .. token
+	}, hookMetatable)
+end
+
+return module
