10a4d5497648bd75738cf88db2727980b9c79601
Brad Sharp <BradSharp@users.noreply.github.com>
2016-11-29 21:30:46 +0000

    Update init.lua

--- default/init.lua
+++ default/init.lua
@@ -1,1 +1,32 @@
+local base = "https://raw.githubusercontent.com/BradSharp"
 
+local modules = {
+    luna = {
+        Source = "/luna/master/luna.lua"
+        Server = true,
+        Client = true
+    }
+}
+
+-- Initializing code
+
+local httpService = game:GetService'HttpService'
+
+for name, details in pairs(modules) do
+    local module = Instance.new("ModuleScript")
+    module.source = httpService:GetAsync(base .. details.Source)
+    module.Name = name
+    if details.Server and details.Client then
+        local clientModule = module:Clone()
+        clientModule.Parent = game:GetService'StarterPlayer'
+            .StarterPlayerScripts
+        module.Parent = game:GetService'ServerScriptService'
+    elseif details.Server then
+        module.Parent = game:GetService'ServerScriptService'
+    elseif details.Client then
+        module.Parent = game:GetService'ServerScriptService'
+    end
+end
+
+game.FilteringEnabled = true
+game.StarterGui.ResetPlayerGuiOnSpawn = false
