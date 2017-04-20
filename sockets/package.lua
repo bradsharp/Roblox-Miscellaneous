f7ea9b2fa1ef086069818ae7ed6e4d6c8d800327
Brad Sharp <BradSharp@users.noreply.github.com>
2017-04-20 14:47:14 +0100

    Update package.lua

--- sockets/package.lua
+++ sockets/package.lua
@@ -1,3 +1,11 @@
 return {
-
+	Name = "Sockets",
+	Version = "1.1",
+	Description = "Alternative client-server communication",
+	Author = {
+		Name = "woot3",
+		UserId = 1451803
+	},
+	License = "MIT",
+	Main = "server",
 }
