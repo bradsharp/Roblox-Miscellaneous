cfdb93a51c7228343ea895bd7203736e2bb94ba3
Brad Sharp <BradSharp@users.noreply.github.com>
2017-04-20 16:22:43 +0100

    Create package.lua

---
+++ sockets/client/package.lua
@@ -0,0 +1,15 @@
+return {
+	Name = "Socket",
+	Version = "1.1",
+	Description = "Alternative client-server communication, client module",
+	Author = {
+		Name = "woot3",
+		UserId = 1451803
+	},
+	License = "MIT",
+	Main = "client",
+	Platform = {
+    Client = true,
+		Server = false
+	}
+}
