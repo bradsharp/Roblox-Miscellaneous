4ac57b568eb7074c00b14a427abe1302e3c204f2
Brad Sharp <BradSharp@users.noreply.github.com>
2021-01-29 15:19:45 +0000

    Added text support

--- Gizmo/gizmo.lua
+++ Gizmo/gizmo.lua
@@ -1,6 +1,6 @@
 ------------------------------------------------------------------------------------------------------------------------
 -- Name:		gizmo.lua
--- Version:		1.0 (1/21/2021)
+-- Version:		1.1 (1/29/2021)
 -- Author:		Brad Sharp
 --
 -- Repository:	https://github.com/BradSharp/Roblox-Miscellaneous/tree/master/Gizmo
